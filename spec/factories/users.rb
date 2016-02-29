71054cafb5be6ca7ef86f7ff4144a841e02ec6ae
Carlos Ramos <carakan@gmail.com>
2016-02-29 13:07:33 -0400

    rails_apps_composer: set up database

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-    provider "MyString"
-    uid "MyString"
+    name "Test User"
+    email "test@example.com"
+    password "please123"
+
+    trait :admin do
+      role 'admin'
+    end
+
   end
 end
